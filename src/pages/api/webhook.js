24608c54d2228f1d1dbd655eb6a0477d54b3192a
acetrondi <kailashrathod993@gmail.com>
2023-01-13 20:09:00 +0530

    added axios

--- src/pages/api/webhook.js
+++ src/pages/api/webhook.js
@@ -1,5 +1,5 @@
 export default function handler(req, res) {
-    //const token = process.env.TOKEN;
+    const token = process.env.TOKEN;
     const mytoken = process.env.WA_MYTOKEN;
     if (req.method === 'GET') {
         let mode = req.query["hub.mode"];
@@ -18,9 +18,47 @@ export default function handler(req, res) {
         }
     }
     if (req.method == 'POST') {
-        let body_param = req.body;
+        let body_param=req.body;
 
-        console.log(JSON.stringify(body_param, null, 2));
+        console.log(JSON.stringify(body_param,null,2));
+    
+        if(body_param.object){
+            console.log("inside body param");
+            if(body_param.entry && 
+                body_param.entry[0].changes && 
+                body_param.entry[0].changes[0].value.messages && 
+                body_param.entry[0].changes[0].value.messages[0]  
+                ){
+                   let phon_no_id=body_param.entry[0].changes[0].value.metadata.phone_number_id;
+                   let from = body_param.entry[0].changes[0].value.messages[0].from; 
+                   let msg_body = body_param.entry[0].changes[0].value.messages[0].text.body;
+    
+                   console.log("phone number "+phon_no_id);
+                   console.log("from "+from);
+                   console.log("boady param "+msg_body);
+    
+                   axios({
+                       method:"POST",
+                       url:"https://graph.facebook.com/v13.0/"+phon_no_id+"/messages?access_token="+token,
+                       data:{
+                           messaging_product:"whatsapp",
+                           to:from,
+                           text:{
+                               body:"Hi.. I'm Prasath, your message is "+msg_body
+                           }
+                       },
+                       headers:{
+                           "Content-Type":"application/json"
+                       }
+    
+                   });
+    
+                   res.sendStatus(200);
+                }else{
+                    res.sendStatus(404);
+                }
+    
+        }
 
     }
 }
